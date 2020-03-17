d70bffcf5b68c8f968964f13fd6233185814aa4b
AddjoyBarreto <addjoybarreto26@gmail.com>
2020-03-17 08:27:37 +0530

    first commit

---
+++ src/Persons/Person.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Person =()=>{
+
+        return (
+            <div style={{alignItems:'center',backgroundColor:'blue'}}>
+                <input type='text' />
+                <button >submit</button>
+                
+            </div>
+        )
+    
+}
+
+export default Person;
